a6f5f0a0dedf555c706c8d8f93731f88d3e250ca
Mike Azarov <draftpodium@gmail.com>
2018-02-11 14:43:50 +0000

    sdfsdfsdf

--- init.sh
+++ init.sh
@@ -1,15 +1,13 @@
-web="/home/box/web"
-sudo mkdir ${web}/
-sudo mkdir ${web}/public/
-sudo mkdir ${web}/public/img/
-sudo mkdir ${web}/public/css/
-sudo mkdir ${web}/public/js/
-sudo mkdir ${web}/uploads/
-sudo mkdir ${web}/etc/
-sudo chown -R box:box ${web}/
+
+
+
+
+
+
+
 
 git config --global user.name "Mike Azarov"
 git config --global user.email draftpodium@gmail.com
 
-touch ${web}/etc/nginx.conf
+
 sudo ln -s ${web}/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
