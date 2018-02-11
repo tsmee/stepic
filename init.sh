b1761c6ba4a874af4e97de19d78aa45431b6c1f4
Mike Azarov <draftpodium@gmail.com>
2018-02-11 13:56:20 +0000

    added nginx.conf to the project folder

--- init.sh
+++ init.sh
@@ -5,8 +5,11 @@ sudo mkdir ${web}/public/img/
 sudo mkdir ${web}/public/css/
 sudo mkdir ${web}/public/js/
 sudo mkdir ${web}/uploads/
-
+sudo mkdir ${web}/etc/
 sudo chown -R box:box ${web}/
 
 git config --global user.name "Mike Azarov"
 git config --global user.email draftpodium@gmail.com
+
+touch ${web}/etc/nginx.conf
+sudo ln -s ${web}/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
