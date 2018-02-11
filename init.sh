5fe1f21a2c276413ba74206b297c2d2e85643b25
Mike Azarov <draftpodium@gmail.com>
2018-02-11 15:48:13 +0000

    sdfsdfsdf

--- init.sh
+++ init.sh
@@ -9,5 +9,6 @@
 git config --global user.name "Mike Azarov"
 git config --global user.email draftpodium@gmail.com
 
-
-sudo ln -s /homebox/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo rm -rf /etc/nginx/sites-enabled/default
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo /etc/init.d/nginx restart
