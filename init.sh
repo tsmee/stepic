87cc6a698e3baf744503e9fb749d81d8ed63e868
Mike Azarov <draftpodium@gmail.com>
2018-02-11 13:44:12 +0000

    it's working

--- init.sh
+++ init.sh
@@ -1,8 +1,9 @@
-web = "/home/box/web"
-sudo mkdir $web/public
-sudo mkdir $web/public/img
-sudo mkdir $web/public/css
-sudo mkdir $web/public/js
-sudo mkdir $web/uploads
-sudo mkdir $web/uploads
-sudo chown -R box:box $web
+web="/home/box/web"
+sudo mkdir ${web}/
+sudo mkdir ${web}/public/
+sudo mkdir ${web}/public/img/
+sudo mkdir ${web}/public/css/
+sudo mkdir ${web}/public/js/
+sudo mkdir ${web}/uploads/
+
+sudo chown -R box:box ${web}/
