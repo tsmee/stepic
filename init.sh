21692c3a79473e7ed0789029416498b971df0269
Mike Azarov <draftpodium@gmail.com>
2018-02-11 13:29:20 +0000

    added directory creating script

---
+++ init.sh
@@ -0,0 +1,8 @@
+web = "/home/box/web"
+sudo mkdir $web/public
+sudo mkdir $web/public/img
+sudo mkdir $web/public/css
+sudo mkdir $web/public/js
+sudo mkdir $web/uploads
+sudo mkdir $web/uploads
+sudo chown -R box:box $web
